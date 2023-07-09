4c6e9b77f7b27023be4d9e450bfe6dacab244911
Tonatiuh Rodriguez <tonatiuh@tcrodriguez.com>
2023-07-08 20:29:25 -0700

    Change API baseURL to non-prod;

--- src/axios-http.ts
+++ src/axios-http.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://api.trainingmode.gg/v1'
+    // baseURL: 'https://api.trainingmode.gg/v1'
+    baseURL: 'http://127.0.0.1:8000/v1'
 });
