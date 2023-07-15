9ca2bce89490f65451e7a8c2ffe2f226f8da7e0f
Tonatiuh Rodriguez <tonatiuh@tcrodriguez.com>
2023-07-15 14:53:38 -0700

    Add combo note list component;

---
+++ src/components/CharacterComboNoteList.vue
@@ -0,0 +1,32 @@
+
+<script lang="ts" setup>
+
+
+const props = defineProps({
+    comboId: Number,
+    comboNotes: Array
+})
+</script>
+<template lang="">
+    <div>
+        <div>
+            <ul>
+                <li
+                    v-for="characterComboNote in comboNotes" 
+                    :key="characterComboNote.id"
+                    class="border rounded p-2 flex flex-col"
+                >
+                    <div class="font-bold">
+                        {{characterComboNote.title}}
+                    </div>
+                    <div>
+                        {{characterComboNote.body}}
+                    </div>
+                </li>
+            </ul>
+        </div>    
+    </div>
+</template>
+<style lang="">
+    
+</style>
