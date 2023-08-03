6e73d456eda97298c7932e7ed2c918179203e09a
Tonatiuh Rodriguez <34406649+TCRodriguez@users.noreply.github.com>
2023-08-03 12:58:03 -0700

    cleanup note component;

--- src/components/Note.vue
+++ src/components/Note.vue
@@ -1,9 +1,8 @@
-<script lang="ts">
-export default {
-    props: {
-        note: Object
-    }
-}
+<script lang="ts" setup>
+import ResourceTagsList from './ResourceTagsList.vue';
+const props = defineProps({
+    note: Object
+});
 </script>
 <template lang="">
     <div>
@@ -11,36 +10,9 @@ export default {
 
             <h3 class="font-bold text-lg">{{note.title}}</h3>
             <p>{{note.body}}</p>
-            <div v-for="tag in note.tags" class="text-blue font-bold">#{{tag.name}}</div>
         </div>
-        <!-- <div v-if="authStore.loggedInUser !== null" >
-            <p class="font-bold">Tags</p>
-            <div class="flex flex-row space-x-2 items-center flex-wrap">
-                <div
-                    v-for="(tag, index) in note.tags" 
-                    :key="index" 
-                    class="flex flex-row items-center"
-                    :class=" {'p-1 rounded': editTagsActive.includes(moveId)}"
-                >
-                    <div class="flex flex-row">
-                        <div>
-                            <span>#{{tag.name}}</span>
-                        </div>
-                        <CloseIcon v-if="editTagsActive.includes(moveId)" class="h-6 w-6" @click="$emit('triggerRemoveTag', tag.id, moveId)" />
-                    </div>
-                </div>
-                <div class="">
-                    <input 
-                        v-if="editTagsActive.includes(moveId)"
-                        ref="addTagInput"
-                        @keyup.enter="$emit('saveTag', addTagInput.value, moveId), addTagInput.value = ''"
-                        type="text" 
-                        placeholder="Enter Tag..."
-                        class="border w-min"
-                    >
-                </div>
-            </div>
-        </div> -->
+        <!-- * Add ResourceTagsList component here -->
+
     </div>
 </template>
 <style lang="">
