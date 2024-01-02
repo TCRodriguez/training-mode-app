2e65676c2440b1781cb47af053ba0eac8fe1399b
Tonatiuh Rodriguez <34406649+TCRodriguez@users.noreply.github.com>
2024-01-02 15:59:08 -0800

    misc updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,16 @@ module.exports = {
   theme: {
     colors: {
       'blue': '#023E6F',
+      'apex-blue': '#181E5B',
       'gray': '#A3A39A',
+      'apex-gray': '#E6C900',
       // 'gray': '#A3A49B',
       'green': '#37981F',
+      'apex-green': '#00B825',
       'yellow': '#F8AF04',
+      'apex-yellow': '#E6C900',
       'red': '#E30000',
+      'apex-red': '#D72638',
       'black': '#0B0B0A',
       'white': '#FFFFFC'
     },
