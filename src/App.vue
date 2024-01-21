fde0f2170613d1e3043c08f386f335ebb6770a08
Tonatiuh Rodriguez <34406649+TCRodriguez@users.noreply.github.com>
2024-01-21 15:44:59 -0800

    add toast message feature

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,56 @@
 <script lang="ts">
-import { RouterLink, RouterView } from "vue-router";
+import { RouterLink, RouterView, useRoute, useRouter } from "vue-router";
 import NavBar from './components/NavBar.vue';
 import { useAuthStore } from "./stores/AuthStore";
 import { useGameStore } from "./stores/GameStore";
 import { useCharacterStore } from "./stores/CharacterStore";
-import { getCharacterId, getGameId } from "./common/helpers";
+import { getCharacterId, getGameId, closeMenu } from "./common/helpers";
+import { watch, onMounted, onUnmounted } from "vue";
+import { showToast } from "./common/helpers";
+import { toast, type ToastOptions } from 'vue3-toastify';
+import { useNavigationStore } from "./stores/NavigationStore";
 
 export default {
   setup() {
-    const loginStore = useAuthStore();
+    const authStore = useAuthStore();
     const gameStore = useGameStore();
     const characterStore = useCharacterStore();
+    const navigationStore = useNavigationStore();
+    const route = useRoute();
+    const router = useRouter();
+
+    const handleClickOutsideMenuModal = (event: any) => {
+      if (event.target.id === 'menu-overlay') {
+        closeMenu();
+      }
+    }
+
+    onMounted(() => {
+      window.addEventListener('click', handleClickOutsideMenuModal);
+    });
+
+    onUnmounted(() => {
+      window.removeEventListener('click', handleClickOutsideMenuModal);
+    });
+  
+    watch(route, (to, from) => {
+      // Check for `status` and `message` query parameters
+      if (to.query.status && to.query.message) {
+        if (to.query.status === 'success' && to.query.message === 'Email successfully verified.') {
+          toast.success("Email verification successful! You may now log in.", {
+            autoClose: 5000,
+            position: toast.POSITION.TOP_CENTER,
+            onClose: () => {
+              return router.replace({ path: to.path, query: {} });
+            }
+          } as ToastOptions);
+          navigationStore.openMenuModalContainer();
+          authStore.openLoginForm();
+        } else if (to.query.status === 'failed') {
+          showToast('There was a problem with the email verification.', 5000, 'error');
+        }
+      }
+    });
 
     return {
       gameStore,
--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,11 @@ import {
     regex
 } from '@vee-validate/rules';
 import { localize } from '@vee-validate/i18n';
+import App from "./App.vue";
+import router from "./router";
+import "./assets/main.css";
+import Vue3Toastify, { type ToastContainerOptions } from "vue3-toastify";
+import 'vue3-toastify/dist/index.css';
 
 defineRule('required', required);
 defineRule('email', email);
@@ -31,14 +36,14 @@ configure({
     }
 });
 
-import App from "./App.vue";
-import router from "./router";
 
-import "./assets/main.css";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(Vue3Toastify, {
+    autoClose: 3000,
+} as ToastContainerOptions);
 
 app.mount("#app");
