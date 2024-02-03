4ee1055e7eb17bdc1eaa425feb2ab6156224e441
Tonatiuh Rodriguez <34406649+TCRodriguez@users.noreply.github.com>
2024-02-03 01:39:13 -0800

    add canned directional inputs;

--- src/components/DirectionalInputSwitcher.vue
+++ src/components/DirectionalInputSwitcher.vue
@@ -1,10 +1,12 @@
 <script lang="ts">
 import DirectionalInputGroup from './DirectionalInputGroup.vue';
 import DirectionalInputHoldGroup from './DirectionalInputHoldGroup.vue';
+import DirectionalInputCanned from './DirectionalInputCanned.vue';
 import { ref } from 'vue';
 export default {
     setup() {
         const directionalInputGroupSelection = ref('tap');
+
         return {
             directionalInputGroupSelection
         }
@@ -12,20 +14,34 @@ export default {
     components: {
         DirectionalInputGroup,
         DirectionalInputHoldGroup,
+        DirectionalInputCanned
     }        
 }
 </script>
 <template lang="">
-    <div class="flex flex-col ">
-        <div class="flex flex-row text-yellow justify-center space-x-2">
-            <button class="border rounded p-2" @click="directionalInputGroupSelection = 'tap'">Tap</button>
-            <button class="border rounded p-2" @click="directionalInputGroupSelection = 'hold'">Hold</button>
+    <div class="flex flex-col" :class="{'xs:w-1/2' : directionalInputGroupSelection === 'group'}">
+        <div 
+            class="flex flex-row text-yellow space-x-2" 
+            :class="{
+                'justify-end' : directionalInputGroupSelection === 'group', 
+                'justify-center': directionalInputGroupSelection === 'tap' || directionalInputGroupSelection === 'hold'
+            }"
+        >
+            <button class="p-2" :class="{'border rounded': directionalInputGroupSelection === 'tap'}" @click="directionalInputGroupSelection = 'tap'">Tap</button>
+            <button class="p-2" :class="{'border rounded': directionalInputGroupSelection === 'hold'}" @click="directionalInputGroupSelection = 'hold'">Hold</button>
+            <button class="p-2" :class="{'border rounded': directionalInputGroupSelection === 'group'}" @click="directionalInputGroupSelection = 'group'">Group</button>
         </div>
         <div>
-            <DirectionalInputGroup v-if="directionalInputGroupSelection === 'tap'" />
-        </div>
-        <div>
-            <DirectionalInputHoldGroup v-if="directionalInputGroupSelection === 'hold'" />
+
+            <div v-if="directionalInputGroupSelection === 'tap'">
+                <DirectionalInputGroup />
+            </div>
+            <div v-if="directionalInputGroupSelection === 'hold'">
+                <DirectionalInputHoldGroup />
+            </div>
+            <div v-if="directionalInputGroupSelection === 'group'" class="overflow-y-auto xs:h-[10rem] lg:h-[13rem]">
+                <DirectionalInputCanned  />
+            </div>
         </div>
 
     </div>
